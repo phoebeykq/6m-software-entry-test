b3657e70037dcb81c2a6f0f14c1d3c328f37313c
Phoebe <113828692+phoebeykq@users.noreply.github.com>
2022-10-08 06:03:05 +0800

    Add files via upload

---
+++ src/q5-Phoebe-ans-INCOMPLETE.js
@@ -0,0 +1,18 @@
+/*
+    Task 1:
+    - Import the file `external.js`.
+    - Destructure the imported function.
+*/
+
+// Task 1: Add code here
+
+const printer = require("./external.js"); 
+
+console.log(`${printer.print()}`); 
+
+
+
+/*
+- Destructure the imported function.
+not sure how to do this?
+*/
